9d7a7dec795c14bdefb29bc446af7c0b151a765b
Mark Wardle <mark@wardle.org>
2023-05-21 11:27:12 +0100

    Update SnomedController.java

--- crossmatch-web/src/main/java/com/nhshackday/crossmatchweb/controller/SnomedController.java
+++ crossmatch-web/src/main/java/com/nhshackday/crossmatchweb/controller/SnomedController.java
@@ -1,9 +1,12 @@
 package com.nhshackday.crossmatchweb.controller;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.nhshackday.crossmatchweb.model.Project;
 import com.eldrix.hermes.client.Hermes;
+import com.eldrix.hermes.sct.IResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,15 +20,34 @@ public class SnomedController {
   private Hermes hermes;
   private static final List<Project> PROJECTS = Project.ALL;
 
-  @GetMapping({"/snomed/search"})
-  public String getSnomedSearch(@RequestParam(value = "name") String name, @RequestParam(value = "s") String s, @RequestParam(value = "ecl") String ecl) {
-    String sb = "<select " +
+
+  private static String makeOption(IResult result) {
+    return "<option value=\"" +
+            result.id() +
+            "\">" +
+            result.term() +
+            "</option>";
+  }
+
+  private static String makeOptions(List<IResult> results) {
+    return results.stream().map(SnomedController::makeOption).collect(Collectors.joining());
+  }
+
+  private static String makeSelect(String name, List<IResult> results) {
+    return "<select " +
             "name=\"" +
             name +
             "\" id=\"" +
             name +
             "\">" +
+            makeOptions(results) +
             "</select>";
-    return sb;
+  }
+
+
+  @GetMapping({"/snomed/search"})
+  public String getSnomedSearch(@RequestParam(value = "name") String name, @RequestParam(value = "s") String s, @RequestParam(value = "ecl") String ecl) {
+    List<IResult> results = Collections.emptyList(); // TODO: call autocomplete service
+    return makeSelect(name, results);
   }
 }
