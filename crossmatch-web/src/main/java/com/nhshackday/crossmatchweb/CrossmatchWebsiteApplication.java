b2e95170997457149af4af2a4a88cc805eb26a52
Mark Wardle <mark@wardle.org>
2023-05-21 10:26:42 +0100

    Example of use of SNOMED from Java

--- crossmatch-web/src/main/java/com/nhshackday/crossmatchweb/CrossmatchWebsiteApplication.java
+++ crossmatch-web/src/main/java/com/nhshackday/crossmatchweb/CrossmatchWebsiteApplication.java
@@ -1,5 +1,7 @@
 package com.nhshackday.crossmatchweb;
 
+import com.eldrix.hermes.client.Hermes;
+import com.eldrix.hermes.sct.IResult;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CrossmatchWebsiteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CrossmatchWebsiteApplication.class, args);
+
+		// example
+		Hermes hermes = Hermes.openLocal( "/Users/mark/Dev/hermes/snomed.db");
+		Hermes.SearchRequest request = Hermes.newSearchRequestBuilder().setFuzzy(0).setFallbackFuzzy(2).setS("parkins").setConstraint("<64572001").setMaxHits(500).build();
+
+		for (IResult result : hermes.search(request)) {
+			System.out.println(result.term());
+		};
+
+		//SpringApplication.run(CrossmatchWebsiteApplication.class, args);
 	}
 
 }
